824176fd33ac795ea1f14906a30adb5742d09786
Dexter <112763699+dexter-90@users.noreply.github.com>
2023-04-21 20:52:04 +0300

    Update lib.py

--- lib.py
+++ lib.py
@@ -1,5 +1,7 @@
+import gdown
 import requests
 from bs4 import BeautifulSoup
+from tqdm import tqdm
 
 
 class Witanime:
@@ -19,7 +21,6 @@ class Witanime:
 
             result = {}
             for tc, tc2, uc in zip(title_cards, type_cards, url_cards):
-
                 card = tc.find("a")
                 result[card.text] = {"url": uc["href"], "type": tc2.find("a").text.strip()}
             return result
@@ -34,14 +35,16 @@ class Witanime:
         return result
 
     @staticmethod
-    def Episode_Link(episode, Type=1):
+    def Episode_Link(episode: str, Type=1):
         if Type == 1:
-            soup = BeautifulSoup(requests.get(str(episode)).content, 'html.parser')
+            response = requests.get(episode).content
+            soup = BeautifulSoup(response, 'html.parser')
             result = {quality.find('li').text: {l.find('a').text: l.a['href'] for l in quality.find_all('li')[1:]} for
                       quality in soup.find_all('ul', class_='quality-list')}
             return result
         elif Type == 2:
-            soup = BeautifulSoup(requests.get(str(episode)).content, 'html.parser')
+            url = str(episode)
+            soup = BeautifulSoup(requests.get(url).content, 'html.parser')
             result = {i.text: i.get("href") for i in soup.find_all("a", class_="btn btn-default")}
             return result
 
@@ -61,11 +64,15 @@ class Witanime:
         episode_duration = anime_info[2].text.strip()
         premiered = anime_info[1].text.strip()
         episodes = Witanime.Episodes(url)
-        episodes_count = anime_info[3].text.strip()
+        episodes_count = len(episodes)
         season = anime_info[4].text.strip()
         source = anime_info[5].text.strip()
         mal = soup.find('a', class_='anime-mal')['href']
-        teaser = soup.find('a', class_='anime-trailer')['href']
+        try:
+            teaser = soup.find('a', class_='anime-trailer')['href']
+        except TypeError:
+            teaser = None
+
         is_movie = True if "Movie" in anime_type else False
 
         return {"title": title, "story": story, "genres": genres, "type": anime_type, "status": status, "cover": cover,
@@ -74,7 +81,47 @@ class Witanime:
                 "is-movie": is_movie}
 
     @staticmethod
-    def Download(episodes):
-        pass
+    def Download(url, fn):
+        global response
+        if "mega" in url:
+            mega_key = url.split("#")[1]
+            mega_cipher = mega_key.split("!")
+            file_id = url.split("/")[-1].split("#")[0]
 
+            download_link = f"https://mega.nz/#!{mega_cipher[0]}!{mega_key}!{file_id}"
 
+            response = requests.get(download_link, stream=True)
+
+            with open(fn, "wb") as f:
+                file_size = int(response.headers.get("Content-Length", 0))
+                chunk_size = 1024
+                progress_bar = tqdm(total=file_size, unit="iB", unit_scale=True)
+
+                for chunk in response.iter_content(chunk_size=chunk_size):
+                    if chunk:
+                        f.write(chunk)
+                        progress_bar.update(len(chunk))
+
+            progress_bar.close()
+
+        elif "drive" in url:
+            gdown.download(url, fn, quiet=False)
+
+        elif "file upload" in url or "mediafire" in url:
+
+            response = requests.get(url, stream=True)
+
+            with open(fn, "wb") as f:
+                file_size = int(response.headers.get("Content-Length", 0))
+                chunk_size = 1024
+                progress_bar = tqdm(total=file_size, unit="iB", unit_scale=True)
+
+                for chunk in response.iter_content(chunk_size=chunk_size):
+                    if chunk:
+                        f.write(chunk)
+                        progress_bar.update(len(chunk))
+
+            progress_bar.close()
+
+        else:
+            print("Can't Find Video Server URL")
